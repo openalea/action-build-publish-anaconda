0bc3f1d1299c777fa5d799813f6b5a0986b55db1
maxibor <maxime.borry@gmail.com>
2019-11-06 18:50:18 +0100

    remove setup.py check

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,13 +9,6 @@ go_to_build_dir() {
     fi
 }
 
-check_if_setup_file_exists() {
-    if [ ! -f setup.py ]; then
-        echo "setup.py must exist in the directory that is being packaged and published."
-        exit 1
-    fi
-}
-
 check_if_meta_yaml_file_exists() {
     if [ ! -f meta.yaml ]; then
         echo "meta.yaml must exist in the directory that is being packaged and published."
@@ -36,7 +29,6 @@ upload_package(){
 }
 
 go_to_build_dir
-check_if_setup_file_exists
 check_if_meta_yaml_file_exists
 build_package
 upload_package
