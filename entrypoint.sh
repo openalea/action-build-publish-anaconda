89a796f0bb9df114fc72937720bf5e54e9f409d9
Maxime Borry <maxibor@users.noreply.github.com>
2020-05-14 14:26:38 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,7 +17,7 @@ check_if_meta_yaml_file_exists() {
 }
 
 build_package(){
-    conda build -c conda-forge --output-folder . .
+    conda build -c conda-forge -c bioconda --output-folder . .
     conda convert -p osx-64 linux-64/*.tar.bz2
 }
 
