ce8d4511e8c3ca0da5efbbf8005679e502852480
thomasarsouze <thomas.arsouze@zoho.com>
2022-03-31 23:35:18 +0200

    Adapt entrypoint.sh to args

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@ set -ex
 set -o pipefail
 
 go_to_build_dir() {
-    if [ ! -z $INPUT_CONDABUILDDIR ]; then
-        cd $INPUT_CONDABUILDDIR
+    if [ ! -z $1 ]; then
+        cd $1
     fi
 }
 
@@ -17,14 +17,14 @@ check_if_meta_yaml_file_exists() {
 }
 
 build_package(){
-    IFS=','; read -a arr_channels<<<"$INPUT_CHANNELS"; unset IFS;
+    IFS=','; read -a arr_channels<<<"$4"; unset IFS;
     channels=""; for c in "${arr_channels[@]}"; do channels+="-c $c "; done
-    conda build ${channels} --python=$INPUT_PYTHONVERSION --output-folder . .
+    conda build ${channels} --python=$2 --output-folder . .
 }
 
 upload_package(){
-    export ANACONDA_API_TOKEN=$INPUT_ANACONDATOKEN
-    anaconda upload --skip-existing --no-progress
+    export ANACONDA_API_TOKEN=$3
+    anaconda upload --skip-existing --no-progress -u $5
 }
 
 go_to_build_dir
